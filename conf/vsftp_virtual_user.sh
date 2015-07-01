fc68eac738b21e50581eb64be70a978b8ac80d3b
Yongbok Kim <ruo91@yongbok.net>
2015-07-01 12:46:37 +0900

    Update conf/vsftp_virtual_user.sh

--- conf/vsftp_virtual_user.sh
+++ conf/vsftp_virtual_user.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-# User
-echo -ne "New user name: "
-read user_id
+vsftpd_username="$1"
+vsftpd_userpass="$2"
+vsftpd_pwd_file="/etc/vsftpd/virtual_user.passwd"
+vsftpd_encrypted_password="`grep $vsftpd_username $vsftpd_pwd_file | cut -d ':' -f 2`"
 
-# Create passwd file
-vsftpd_dir=/etc/vsftpd
-vsftpd_conf=$vsftpd_dir/virtual_user.passwd
-htpasswd -cd $vsftpd_conf $user_id
+# Main
+case $1 in
+    $1)
+        htpasswd -bcd $vsftpd_pwd_file $vsftpd_username $vsftpd_userpass
+        echo
+        echo "User Name          : $vsftpd_username"
+        echo "User Passwd        : $vsftpd_userpass"
+        echo "Encrypted Password : $vsftpd_encrypted_password"
+    ;;
+
+    *)
+        echo "Usage: $0 [USERNAME] [PASSWORD]"
+    ;;
+esac
