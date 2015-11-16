adff3dbedbff20caa81acdf6ab292629b59b545c
mouyigang <mouyigang@gmail.com>
2015-11-16 11:08:51 +0800

    Check password file at runtime

--- conf/vsftpd_pid.sh
+++ conf/vsftpd_pid.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #Update user id to match with mounted folder, pass the permission issue
 sudo usermod -u $(stat -c "%u" /home) vsftpd
+
+# Create an empty passwd file if it's not exit
+[ -f /etc/vsftpd/vuser.passwd ] && touch /etc/vsftpd/vuser.passwd
 #Deamon
 /usr/bin/supervisord
